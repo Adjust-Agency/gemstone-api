f8470fb621e07ecd4b09f8f56094720613f684ae
lepinse <lepinse@gmail.com>
2015-02-23 15:15:18 +0100

    psr-4 autloader

--- autoload.php
+++ autoload.php
@@ -1,8 +1,29 @@
 <?php
-spl_autoload_register(function ($className) {
-    $fileParts = explode('\\', ltrim($className, '\\'));
-    if (false !== strpos(end($fileParts), '_'))
-    array_splice($fileParts, -1, 1, explode('_', current($fileParts)));
-    array_shift($fileParts);
-    require __DIR__.'/src/'.implode(DIRECTORY_SEPARATOR, $fileParts) . '.php';
-}); 
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    $prefix = 'Adjust\\GemstoneApi\\';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/src/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
